592226add69f0cf7e49d7a3fc5835ce6c7c78f8a
SuzanneAdams <suzanneadams360@gmail.com>
2016-04-02 19:17:31 -0400

    new git init on PC

---
+++ server.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var ejs = require('ejs');
+var request = require('request');
+var cheerio = require('cheerio');
+var url = "https://www.reddit.com/";
+var express = require("express");
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+
+try{
+  var env = require('./config/env_dev');
+}
+catch(err){
+  var env = require('./config/env_prod');
+}
+
+
+
+app.get('/', function(req, res) {
+  var itemArray = [];
+
+    request(url, function (error, response, body) {
+
+      if (!error) {
+        var $ = cheerio.load(body);
+
+          $('a.title').each(function(i, elm) {
+          // console.log($(this).html());
+          item = $(this).text();
+          itemArray.push(item);
+    		  });
+
+      } else {
+        console.log("We’ve encountered an error: " + error);
+      }
+
+    // console.log(itemArray);
+    res.render('index', { itemArray: itemArray });
+    });
+});
+
+
+app.listen(env.port,function(){
+  console.log('Listening on '+env.host+':'+env.port);
+  console.log('Stop Server With CTRL + C');
+});
+
+
